486c9dd07b66eaea033145aeef6538d468c6ea70
birukax <birukax@gmail.com>
2024-12-28 17:59:52 +0300

    Revert "First Commit"
    This reverts commit c6ba72fc78a32528b17f06295bdb690171fe1314.

---
+++ job/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Job(models.Model):
+    no = models.CharField(max_length=100)
+    tests = models.IntegerField(default=1)
+    product = models.ForeignKey("misc.Product", on_delete=models.CASCADE)
+    customer = models.ForeignKey(
+        "misc.Customer", on_delete=models.CASCADE, null=True, blank=True
+    )
+    machine = models.ForeignKey(
+        "misc.Machine", on_delete=models.CASCADE, null=True, blank=True
+    )
+    color_standard = models.ForeignKey(
+        "misc.ColorStandard", on_delete=models.CASCADE, null=True, blank=True
+    )
+    certificate_no = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        ordering = ["-no"]
+
+    def __str__(self):
+        return f"{self.no } - {self.product.name}"
+
+    def get_absolute_url(self):
+        return reverse("job:detail", args={self.id})
---
+++ pallet/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import PalletCard, PalletMachine
+
+admin.site.register(PalletCard)
+admin.site.register(PalletMachine)
