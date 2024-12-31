4d7c00e37aae07f1a40c23b971560382c6524fdf
birukax <birukax@gmail.com>
2024-12-31 12:24:37 +0300

    Added modal for the first off approval detail

--- job/tasks.py
+++ job/tasks.py
@@ -14,29 +14,29 @@ def get_job():
     user = config("NAV_INSTANCE_USER")
     password = config("NAV_INSTANCE_PASSWORD")
     auth = HttpNtlmAuth(user, password)
-    # try:
-    response = requests.get(url, auth=auth)
-    if response.ok:
-        data = response.json()
-        for job in data["value"]:
-            product = Product.objects.filter(no=job["Source_No"])
-            if product.exists():
-                job = Job(
-                    no=job["No"],
-                    product=product.first(),
-                )
-                job_exists = Job.objects.filter(no=job.no)
-                if job_exists.exists():
-                    job_exists.update(
-                        product=Product.objects.get(no=job.product.no),
+    try:
+        response = requests.get(url, auth=auth)
+        if response.ok:
+            data = response.json()
+            for job in data["value"]:
+                product = Product.objects.filter(no=job["Source_No"])
+                if product.exists():
+                    job = Job(
+                        no=job["No"],
+                        product=product.first(),
                     )
-                else:
-                    job.save()
-    # except Exception as e:
-    #     print(e)
+                    job_exists = Job.objects.filter(no=job.no)
+                    if job_exists.exists():
+                        job_exists.update(
+                            product=Product.objects.get(no=job.product.no),
+                        )
+                    else:
+                        job.save()
+    except Exception as e:
+        print(e)
 
 
-def create_first_offs(request,id):
+def create_first_offs(request, id):
     job = get_object_or_404(Job, id=id)
     if request.method == "POST":
         form = CreateFirstOffForm(request.POST)
